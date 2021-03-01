04de4134b851ac381b16661bd911128f37c6e6b3
mitsumushibayama <mitsumushibayama@icloud.com>
2021-03-01 15:29:37 +0900

    add get_specified_color_characters()

--- charadb.py
+++ charadb.py
@@ -24,6 +24,29 @@ def get_all_characters():
 		json_response = { "character_data" : response_list }			
 		
 		return json_response
+def get_specified_color_characters(color):
+
+	connector = pymysql.connect(
+		host = config.host,
+		user = config.user,
+		passwd = config.passwd,
+		db = config.db,
+		charset = config.charset,
+		cursorclass = pymysql.cursors.DictCursor)
+
+	with connector.cursor() as cursor:
+		sql = 'select * from characters where attribute = %s;'% (color)
+		cursor.execute(sql)
+		result = cursor.fetchall()
+
+		response_list = []
+
+		for i in range(len(result)):
+			response_list.append(result[i])
+
+		json_response = { "character_data" : response_list }
+
+		return json_response
 
 	
 			
--- server.py
+++ server.py
@@ -8,6 +8,9 @@ app = FastAPI()
 def get_all_characters():
 	return charadb.get_all_characters()
 
+@app.get("/characters/{color}")
+def get_specified_color_characters(color : str):
+        return charadb.get_specified_color_characters(color)
 
 
 
