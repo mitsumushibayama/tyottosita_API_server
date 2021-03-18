d649ff84fcd58d9174e9145ca60cca25cfda5100
mitsumushibayama <mitsumushibayama@icloud.com>
2021-03-18 10:15:30 +0900

    fixed @app.get("/characters/skill/")

--- server.py
+++ server.py
@@ -12,9 +12,9 @@ async def get_all_characters():
 async def get_specified_color_characters(color: str):
         return charadb.get_specified_color_characters(color)
 
-@app.get("/characters/skill/type/{type}/cut/{cut}")
-async def get_skill_characters(type: str, cut: int):
-	return charadb.get_skill_characters(type, cut)
+@app.get("/characters/skill/")
+async def get_skill_characters(type: str, cut: int = None, flag: bool = True):
+	return charadb.get_skill_characters(type, cut, flag)
 
 
 if __name__ == "__main__":
