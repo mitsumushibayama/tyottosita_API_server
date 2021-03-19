b0a778c98da10c5a358c95bd6af4eff4edec9600
mitsumushibayama <mitsumushibayama@icloud.com>
2021-03-19 22:59:53 +0900

    sqlの処理の変更

--- charadb.py
+++ charadb.py
@@ -14,14 +14,9 @@ def get_all_characters():
 	with connector.cursor() as cursor:
 		sql = "select * from characters;"
 		cursor.execute(sql)
-		result = cursor.fetchall()
+		sql_result = cursor.fetchall()
 
-		response_list = []
-
-		for i in range(len(result)):
-			response_list.append(result[i])
-
-		json_response = { "character_data" : response_list }			
+		json_response = { "character_data" : sql_result }			
 		
 		return json_response
 
@@ -39,14 +34,9 @@ def get_specified_color_characters(color):
 	with connector.cursor() as cursor:
 		sql = 'select * from characters where color = "%s";'% (color)
 		cursor.execute(sql)
-		result = cursor.fetchall()
+		sql_result = cursor.fetchall()
 
-		response_list = []
-
-		for i in range(len(result)):
-			response_list.append(result[i])
-
-		json_response = { "character_data" : response_list }
+		json_response = { "character_data" : sql_result }
 
 		return json_response
 
@@ -67,14 +57,9 @@ def get_skill_characters(type, cut, flag):
 
 			sql = 'select characters.stars, characters.color, characters.name, skills.type, skills.cut, skills.target, characters_skill.quantity from characters inner join characters_skill on characters.character_id = characters_skill.character_id inner join skills on characters_skill.skill_id = skills.skill_id where skills.type = "%s" and skills.cut = %s;'% (type, cut)
 			cursor.execute(sql)
-			result = cursor.fetchall()
+			sql_result = cursor.fetchall()
 
-			response_list = []
-
-			for i in range(len(result)):
-				response_list.append(result[i])
-
-			json_response = { "character_data" : response_list }
+			json_response = { "character_data" : sql_result }
 
 			return json_response
 
@@ -82,14 +67,9 @@ def get_skill_characters(type, cut, flag):
 	
 			sql = 'select characters.stars, characters.color, characters.name, skills.type, skills.cut, skills.target, characters_skill.quantity from characters inner join characters_skill on characters.character_id = characters_skill.character_id inner join skills on characters_skill.skill_id = skills.skill_id where skills.type = "%s"'% (type)
 			cursor.execute(sql)
-			result = cursor.fetchall()
+			sql_result = cursor.fetchall()
 
-			response_list = []
-
-			for i in range(len(result)):
-				response_list.append(result[i])
-
-			json_response = { "character_data" : response_list }
+			json_response = { "character_data" : sql_result }
 
 			return json_response
 
